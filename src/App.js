a53619fbe3203e6f7a97746caef271a393f3ad80
Michael kang <michaelkang9898@gmail.com>
2021-09-21 04:20:28 +1200

    test 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from 'react';
+
 function App() {
+    const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch('http://54.153.139.15/jobs').then(
+      response => response.json().then(data => {
+        setJobs(data);
+      })
+    );
+
+
+  }, []);
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<h1>Job listing test</h1>
+	
+		{jobs}
     </div>
   );
 }
